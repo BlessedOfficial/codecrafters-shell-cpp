06b9f881099f3cac9d678352add16347a2943c6f
blessedofficial <blessed@amalgamate.co.za>
2026-08-18 18:44:52 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@ int main() {
     std::cin >> input;
     if(input =="exit"){
       break;
+    }else if(input.substr(0,4) == "echo"){
+      std::cout << input.substr(4) << std::endl;
     }
 
     std::cout << input << ": command not found" <<  std::endl;;
