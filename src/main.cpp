80414f19f7ac0348953535f1993afd22c1cdd6b6
blessedofficial <blessed@amalgamate.co.za>
2026-08-18 18:52:09 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main() {
     {
       break;
     }
-    else if (input.substr(0, 4) == "echo ")
+    else if (input.substr(0, 5) == "echo ")
     {
       std::cout << input.substr(4) << std::endl;
     }
