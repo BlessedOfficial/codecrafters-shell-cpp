31e18fe37d5bb8c9a876cc42d611be9893e7b98a
blessedofficial <blessed@amalgamate.co.za>
2026-08-19 05:31:44 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,15 @@ int main() {
     else if (input.substr(0, 5) == "echo ")
     {
       std::cout << input.substr(5) << std::endl;
+    }else if(input.substr(0, 5) == "type "){
+      if (input.substr(5) == "echo")
+      {
+        std::cout << input.substr(5) <<" is a shell builtin" << std::endl;
+      }else if(input.substr(5) == "exit"){
+      std::cout << input.substr(5) <<" is a shell builtin" << std::endl;
+      }else if(input.substr(5) == "type"){
+      std::cout << input.substr(5) <<" is a shell builtin" << std::endl;
+      }
     }
     else
     {
