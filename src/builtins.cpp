28b1dcb02ebc3f1e71af16c73aac9d112ba8ac46
blessedofficial <blessed@amalgamate.co.za>
2026-09-11 20:34:46 +0200

    codecrafters submit [skip ci]

--- src/builtins.cpp
+++ src/builtins.cpp
@@ -11,15 +11,58 @@ const unordered_set<string> BUILTINS = {"echo", "exit", "type", "pwd", "cd"};
 
 void handle_echo(const Command &cmd)
 {
-    for (size_t i = 1; i < cmd.args.size(); ++i)
+    if (cmd.redirect_stdout)
     {
-        cout << cmd.args[i];
-        if (i + 1 < cmd.args.size())
+        pid_t pid = fork();
+
+        if (pid < 0)
         {
-            cout << " ";
+            perror("fork failed");
+            return;
+        }
+
+        if (pid == 0) // CHILD PROCESS
+        {
+            int fd = open(cmd.stdout_file.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd < 0)
+            {
+                perror("open failed");
+                exit(EXIT_FAILURE);
+            }
+            dup2(fd, STDOUT_FILENO); // STDOUT_FILENO is 1
+            close(fd);
+
+            // Print arguments to redirected stdout
+            for (size_t i = 1; i < cmd.args.size(); ++i)
+            {
+                cout << cmd.args[i];
+                if (i + 1 < cmd.args.size())
+                {
+                    cout << " ";
+                }
+            }
+            cout << '\n';
+
+            exit(EXIT_SUCCESS); // Terminate child execution cleanly
+        }
+        else // PARENT PROCESS
+        {
+            int status;
+            waitpid(pid, &status, 0); // Wait for child process to finish
         }
     }
-    cout << '\n';
+    else // NORMAL ECHO
+    {
+        for (size_t i = 1; i < cmd.args.size(); ++i)
+        {
+            cout << cmd.args[i];
+            if (i + 1 < cmd.args.size())
+            {
+                cout << " ";
+            }
+        }
+        cout << '\n'; // Added missing newline
+    }
 }
 
 void handle_type(const Command &cmd, const vector<string> &paths)
