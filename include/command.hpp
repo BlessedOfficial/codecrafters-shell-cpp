2677949416a864f27df7b560fa58227d395d1359
blessedofficial <blessed@amalgamate.co.za>
2026-09-13 19:19:34 +0200

    codecrafters submit [skip ci]

--- include/command.hpp
+++ include/command.hpp
@@ -8,4 +8,5 @@ struct Command
     std::vector<std::string> args; // clean command and arguments
     std::string stdout_file = "";  // target file for redirection
     bool redirect_stdout = false;  // flag indicating if redirection exists
+    bool redirect_stderr = false; //flag if error redirection 2>
 };
